b5d1274b2d9ea0aaf4c419b82169ba60398a5f43
eduardperazaguardado-cmyk <eduardperazaguardado@gmail.com>
2026-03-20 22:50:11 -0600

    Ejercicio 6 Cree un programa que solicite un nombre completo y lo separe en palabras utilizando el método split(). Luego muestre cada palabra en una línea diferente.

---
+++ Ejercicios_Semana9/nombre_Separado.py
@@ -0,0 +1,5 @@
+nombre = input("Escribe tu nombre completo: ")
+nombre_separado = nombre.split()
+print("Tu nombre separado es: ", nombre_separado)
+for nombre in nombre_separado:
+    print(nombre)
