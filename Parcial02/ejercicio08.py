8e9b6783ef4c053c7e4bd11c51b52e049d66d269
eduardperazaguardado-cmyk <eduardperazaguardado@gmail.com>
2026-04-11 12:36:15 -0600

    Parcial 02 Fundamentos de programacion, ejercicio 8,Escribir un texto de 3 lineas usando comillas triples, contar cuantas veces aparece la letra a en el texto y dividir el bloque del texto y convertirlas en una lista de oraciones independientes

---
+++ Parcial02/ejercicio08.py
@@ -0,0 +1,12 @@
+# 1. Bloque de texto de 3 líneas
+texto = """Gente, hay que empezar a preocuparnos menos y a disfrutar más
+ porque la vida va rápido, demasiado rápido,
+ asi como este año que me lo llevo en el corazon por siempre"""
+
+# 2. Contar cuántas veces aparece la letra "a"
+contador = texto.count("a")
+print("La letra 'a' aparece:", contador, "veces")
+
+# 3. Separar el texto por líneas
+lineas = texto.splitlines()
+print(lineas)
