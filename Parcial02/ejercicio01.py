d1b9fee14559a2e9bb54c5b303fc218119a229d7
eduardperazaguardado-cmyk <eduardperazaguardado@gmail.com>
2026-04-11 12:11:58 -0600

    Parcial 02 Fundamentos de programacion, ejercicio 1, declarar una variable con cadena (elefante) y contar las veces que se repite la letra e

---
+++ Parcial02/ejercicio01.py
@@ -0,0 +1,12 @@
+# guardar el texto con espacios
+animal = "  elefante  "
+
+# quitar los espacios al inicio y al final
+animal = animal.strip()
+
+# contar cuántas veces aparece la letra "e"
+cantidad = animal.count("e")
+
+# Pedir resultados
+print(animal)
+print(cantidad)
