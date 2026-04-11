50e8d4574e56dd1858c0d8675665076d159e40d7
eduardperazaguardado-cmyk <eduardperazaguardado@gmail.com>
2026-04-11 12:31:45 -0600

    Parcial 02 Fundamentos de programacion, ejercicio 6, Texto Su nombre aplicar casefold y verificar si el texto resultante esta compuesto unicamente por caracteres analfabeticos

---
+++ Parcial02/ejercicio06.py
@@ -0,0 +1,12 @@
+# texto original
+texto = "Eduard"
+
+# normalización fuerte (ignorar mayúsculas/minúsculas)
+texto = texto.casefold()
+
+# verificar si solo tiene letras (True o False)
+resultado = texto.isalpha()
+
+# Mostrar resultado
+print(texto)
+print(resultado)
