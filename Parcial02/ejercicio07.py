9f0e87fe175d8a2563b4b099465c8abea84335fa
eduardperazaguardado-cmyk <eduardperazaguardado@gmail.com>
2026-04-11 12:33:57 -0600

    Parcial 02 Fundamentos de programacion, ejercicio 7, Tomar el numero 42, rellenarlo con ceros a la izquierda hasta tener una longitud de 5 caracteres y verificar si el nuevo texto generado termina en 2

---
+++ Parcial02/ejercicio07.py
@@ -0,0 +1,12 @@
+# texto original
+texto = "42"
+
+# rellenar con ceros a la izquierda hasta 5 caracteres
+texto = texto.zfill(5)
+
+# verificar si termina en "2"
+resultado = texto.endswith("2")
+
+# Mostrar resultados
+print(texto)
+print(resultado)
