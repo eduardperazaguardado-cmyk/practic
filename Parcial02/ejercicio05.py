3a365e27c67003b822824b90df7889d554bdb0be
eduardperazaguardado-cmyk <eduardperazaguardado@gmail.com>
2026-04-11 12:28:16 -0600

    Parcial 02 Fundamentos de programacion, ejercicio 5, iniciar cadena pYTHON, invertir mayusculas con minusculas y alinear texto hacia la izquierda en un espacio de 15 caracteres rellenando espacios con asteriscos*

---
+++ Parcial02/ejercicio05.py
@@ -0,0 +1,11 @@
+# cadena original
+texto = "pYTHON"
+
+# cambiar mayúsculas por minúsculas y viceversa
+texto = texto.swapcase()
+
+# alinear a la izquierda en 15 espacios y rellenar con "*"
+texto = texto.ljust(15, "*")
+
+# Mostrar resultado
+print(texto)
