90efac30f04458d954cdfc7e671cd736ac34666d
eduardperazaguardado-cmyk <eduardperazaguardado@gmail.com>
2026-04-11 12:22:58 -0600

    Parcial 02 Fundamentos de programacion, ejercicio 3, Crear la variable ING.Su  nombre, remover el prefijo y convertir en mayusculas

---
+++ Parcial02/ejercicio03.py
@@ -0,0 +1,11 @@
+# guardar el texto
+texto = "ING. Eduard"
+
+# quitar "ING. "
+texto = texto.replace("ING. ", "")
+
+# convertir todo a mayúsculas
+texto = texto.upper()
+
+# Mostrar resultado
+print(texto)
