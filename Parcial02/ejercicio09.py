e9b961d996278df1100d18308d842b2ebb208508
eduardperazaguardado-cmyk <eduardperazaguardado@gmail.com>
2026-04-11 12:38:18 -0600

    Parcial 02 Fundamentos de programacion, ejercicio 09, Tomar la cadena Any time y Anytime, remplazar el que tiene espacio por Always y convertir el texto resultante en mayusculas

---
+++ Parcial02/ejercicio09.py
@@ -0,0 +1,10 @@
+# Cadena original
+texto = "Any time y Anytime"
+
+# Reemplazar "Any time" por "Always"
+texto_requerido = texto.replace("Any time", "Always")
+
+#  Convertir a mayúsculas
+resultado = texto_requerido.upper()
+
+print(resultado)
