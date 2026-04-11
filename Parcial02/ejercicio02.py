87efbaa5a132506de35a6e538acf787aa0f557ec
eduardperazaguardado-cmyk <eduardperazaguardado@gmail.com>
2026-04-11 12:21:10 -0600

    Parcial 02 Fundamentos de programacion, ejercicio 2, Toma la cadena de texto su nombre remplazala por tu apellido con la primer letra en mayuscula

---
+++ Parcial02/ejercicio02.py
@@ -0,0 +1,11 @@
+# guardar el texto
+texto = "Eduard"
+
+# poner mayúscula al inicio de cada palabra
+texto = texto.title()
+
+# cambiar "nombre" por "apellido"
+texto = texto.replace("Eduard", "Peraza")
+
+# Mostrar resultado
+print(texto)
